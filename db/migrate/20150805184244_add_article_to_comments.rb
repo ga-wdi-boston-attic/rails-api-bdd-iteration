dbddabc8586dcdeac2815132c28acf340b42b5e0
Jeffrey Horn <jeffh@generalassemb.ly>
2016-02-23 22:10:23 -0500

    Remove model specs

--- db/migrate/20150805184244_add_article_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddArticleToComments < ActiveRecord::Migration
-  def change
-    add_reference :comments, :article, index: true, foreign_key: true
-  end
-end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,14 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
-  describe 'associations' do
-    def association
-      described_class.reflect_on_association(:article)
-    end
-
-    it 'belongs to an article' do
-      expect(association).to_not be_nil
-      expect(association.options[:inverse_of]).to_not be_nil
-    end
-  end
 end
